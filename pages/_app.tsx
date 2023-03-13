d7b91f253e5e71c5dcebcfe4ac287cf697d18bd5
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2023-03-14 00:31:06 +0200

    scroll to top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,55 @@ import { getSession } from "@/services/auth";
 import { setSession } from "@/store/appSlice";
 import darkThemeOptions from "@/theme/darkThemeOptions";
 import { useEffect, useState } from "react";
+import { Metadata } from "next";
+import ScrollToTop from "@/components/common/ScrollToTop";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Noot",
+    template: "%s | Noot"
+  },
+  description:
+    "Digital Solutions Provider - Our website offers a range of digital solutions for businesses seeking innovative software development, web design, and IT services",
+  openGraph: {
+    title: "Noot",
+    description:
+      "Digital Solutions Provider - Our website offers a range of digital solutions for businesses seeking innovative software development, web design, and IT services",
+    url: "https://noot.ae",
+    siteName: "Noot",
+    images: [
+      {
+        url: "https://noot.ae/og.jpg",
+        width: 1920,
+        height: 1080
+      }
+    ],
+    locale: "en-US",
+    type: "website"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1
+    }
+  },
+  twitter: {
+    title: "Noot",
+    card: "summary_large_image"
+  },
+  icons: {
+    shortcut: "/favicon.ico"
+  },
+  verification: {
+    google: "eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw",
+    yandex: "14d2e73487fa6c71"
+  }
+};
 
 function getActiveTheme(themeMode: Mode, locale: any) {
   return themeMode === "light"
@@ -50,6 +99,7 @@ const MyApp = ({ Component, ...rest }: AppProps) => {
 
   useEffect(() => {
     setActiveTheme(getActiveTheme(selectedTheme, locale!));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedTheme]);
 
   const { store, props } = wrapper.useWrappedStore(rest);
@@ -58,35 +108,46 @@ const MyApp = ({ Component, ...rest }: AppProps) => {
   const { emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <Provider store={store}>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={activeTheme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <GlobalStyles
-            styles={{
-              ".MuiAccordionSummary-root .MuiAccordionSummary-content": {
-                margin: "12px 8px !important",
-              },
-              ".MuiAccordionSummary-root .MuiAccordionSummary-content.Mui-expanded":
-                {
-                  margin: "12px 8px !important",
-                },
-              ".MuiAccordionSummary-root.Mui-expanded": {
-                minHeight: "0 !important",
-              },
-              ".MuiAccordionSummary-root.Mui-expanded .MuiTypography-root": {
-                marginLeft: "0 !important",
-              },
-            }}
-          />
-          <Component {...pageProps} toggleTheme={toggleTheme} />
-        </ThemeProvider>
-      </CacheProvider>
-    </Provider>
+    <html lang="en">
+      <Provider store={store}>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
+          <body>
+            <main>
+              <ThemeProvider theme={activeTheme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <GlobalStyles
+                  styles={{
+                    ".MuiAccordionSummary-root .MuiAccordionSummary-content": {
+                      margin: "12px 8px !important"
+                    },
+                    ".MuiAccordionSummary-root .MuiAccordionSummary-content.Mui-expanded":
+                      {
+                        margin: "12px 8px !important"
+                      },
+                    ".MuiAccordionSummary-root.Mui-expanded": {
+                      minHeight: "0 !important"
+                    },
+                    ".MuiAccordionSummary-root.Mui-expanded .MuiTypography-root":
+                      {
+                        marginLeft: "0 !important"
+                      }
+                  }}
+                />
+                <Component {...pageProps} toggleTheme={toggleTheme} />
+              </ThemeProvider>
+              <ScrollToTop />
+            </main>
+          </body>
+        </CacheProvider>
+      </Provider>
+    </html>
   );
 };
 
@@ -102,7 +163,7 @@ MyApp.getInitialProps = wrapper.getInitialAppProps(
     if (!appContext.router.route.includes(Routes.ACCOUNTS)) {
       const cookieSession: CookieValueTypes = getCookie(Cookies.SESSION, {
         req: appContext.ctx.req,
-        res: appContext.ctx.res,
+        res: appContext.ctx.res
       });
 
       if (cookieSession === undefined) {
@@ -112,13 +173,13 @@ MyApp.getInitialProps = wrapper.getInitialAppProps(
           setCookie(Cookies.SESSION, "true", {
             req: appContext.ctx.req,
             res: appContext.ctx.res,
-            maxAge: 60 * 60 * 24,
+            maxAge: 60 * 60 * 24
           });
         } else {
           setCookie(Cookies.SESSION, "false", {
             req: appContext.ctx.req,
             res: appContext.ctx.res,
-            maxAge: 60 * 60 * 24,
+            maxAge: 60 * 60 * 24
           });
         }
       } else {
