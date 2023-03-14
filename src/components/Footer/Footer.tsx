3a404f07013fc8022923dbecc3b22ba890ed4f4d
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2023-03-14 11:52:35 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,19 +2,77 @@ import {
   Box,
   Container,
   Grid,
-  List,
   ListItem,
   ListItemText,
-  Typography,
+  Typography
 } from "@mui/material";
-
 import Logo from "./Logo";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmailIcon from "@mui/icons-material/Email";
+import TextField from "@mui/material/TextField";
 
 interface Props {
   text: string;
+  icon: string;
+}
+interface NumberingTitleProps {
+  title: string;
+  content: string;
 }
 
-const CustomListItem: React.FC<Props> = ({ text }) => {
+const ContactCircle: React.FC<NumberingTitleProps> = ({ title, content }) => {
+  return (
+    <Box sx={{ mb: 1 }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end"
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            paddingRight: "1rem"
+          }}
+        >
+          <Typography sx={{ fontSize: "0.9rem" }} fontWeight={400}>
+            {content}
+          </Typography>
+        </Box>
+
+        <Grid
+          container
+          justifyContent={"center"}
+          alignItems={"center"}
+          sx={{
+            width: 48,
+            height: 48,
+            color: "common.white",
+            backgroundColor: `${title === "whatsapp" ? "#3F813C" : "#06B7B7"}`,
+            borderRadius: "50%"
+          }}
+        >
+          {title === "whatsapp" ? (
+            <WhatsAppIcon sx={{ fontSize: "2rem" }} />
+          ) : null}
+          {title === "email" ? <EmailIcon sx={{ fontSize: "2rem" }} /> : null}
+          {title === "location" ? (
+            <LocationOnIcon sx={{ fontSize: "2rem" }} />
+          ) : null}
+          {/* <Typography  fontWeight={400}>
+          </Typography> */}
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+const CustomListItem: React.FC<Props> = ({ text, icon }) => {
   return (
     <ListItem sx={{ p: 0, color: "#6D757D" }}>
       <ListItemText>{text}</ListItemText>
@@ -37,45 +95,80 @@ const CustomTitle: React.FC<TitleProps> = ({ text }) => {
 const Footer: React.FC = () => {
   const usefulLinks: any[] = ["TOS", "Privacy Policy", "Refund Policy"];
   const terms: any[] = ["Blog", "Pricing", "About"];
-  const support: any[] = ["Open Support Ticket", "Terms of use", "About"];
+  const support: any[] = [
+    { title: "whatsapp", content: "971564010108+" },
+    { title: "email", content: "info@noot.ae" },
+    {
+      title: "location",
+      content: "العين،ابوظبى،الامارات العربية المتحدة"
+    }
+  ];
 
   return (
     <footer>
-      <Box sx={{ backgroundColor: "#f5f7fa", pt: 8, pb: 6 }}>
+      <Box sx={{ backgroundColor: "#f5f7fa", pt: 2, pb: 2 }}>
         <Container>
-          <Grid container>
-            <Grid item xs={12} md={6} sx={{ mb: 4 }}>
-              <Logo />
-              <Typography sx={{ mt: 3 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                lobortis.
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={6} sx={{ mb: 4 }}>
-              <CustomTitle text={`Useful Links`} />
-              <List disablePadding>
-                {usefulLinks.map((text, index) => (
-                  <CustomListItem text={text} key={index} />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: {
+                md: "row",
+                lg: "row",
+                xs: "column-reverse"
+              }
+            }}
+          >
+            <Box>
+              <Box sx={{ alignSelf: "end" }}>
+                <Typography sx={{ color: "#06B7B7", pb: 2 }}>
+                  Sign up for Special Offers!
+                </Typography>
+                <TextField
+                  label="Email"
+                  id="outlined-size-small"
+                  size="small"
+                  sx={{ color: "#06B7B7" }}
+                />
+              </Box>
+            </Box>
+            {/* -------------------- */}
+            <Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "end",
+                  alignItems: "end",
+                  flexDirection: "column"
+                }}
+              >
+                <Box
+                  sx={{
+                    alignSelf: {
+                      xs: "center",
+                      md: "end",
+                      lg: "end"
+                    },
+                    pb: {
+                      lg: "2rem",
+                      md: "2rem",
+                      xs: "1rem"
+                    }
+                  }}
+                >
+                  <Logo />
+                </Box>
+
+                {support.map((supportInfo, index) => (
+                  <ContactCircle
+                    title={supportInfo?.title}
+                    content={supportInfo?.content}
+                    key={index}
+                  />
                 ))}
-              </List>
-            </Grid>
-            <Grid item xs={12} md={6} sx={{ mb: 4 }}>
-              <CustomTitle text={`Terms`} />
-              <List disablePadding>
-                {terms.map((text, index) => (
-                  <CustomListItem text={text} key={index} />
-                ))}
-              </List>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <CustomTitle text={`Support & Help`} />
-              <List disablePadding>
-                {support.map((text, index) => (
-                  <CustomListItem text={text} key={index} />
-                ))}
-              </List>
-            </Grid>
-          </Grid>
+              </Box>
+            </Box>
+          </Box>
         </Container>
       </Box>
       <Box sx={{ backgroundColor: "rgba(6,183,183,.1)", py: 3 }}>
