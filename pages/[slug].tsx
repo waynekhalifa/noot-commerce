15278ea2844885ef0c134779729a001fdb845f65
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2023-03-17 06:20:19 +0200

    add burger to slug

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,24 +1,38 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import NewHeader from "@/components/NewHeader";
 import HomeSections from "@/components/HomeSections";
 import type { NextPage } from "next";
 
 import Head from "next/head";
+import { useState } from "react";
 
 interface Props {
   toggleTheme?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 const Page: NextPage<Props> = ({ toggleTheme }) => {
+  const [burger, setBurger] = useState<boolean>(false);
+  const openBurgerNav = () => {
+    setBurger(true);
+  };
+  const closeBurgerNav = () => {
+    setBurger(false);
+  };
   return (
     <>
       <Head>
         <title>Noot</title>
         <meta name="description" content={"Noot"} />
       </Head>
-      <Header toggleTheme={toggleTheme} />
-      <HomeSections />
-      <Footer />
+      {/* <Header toggleTheme={toggleTheme} /> */}
+      <NewHeader
+        toggleTheme={toggleTheme}
+        openBurgerNav={openBurgerNav}
+        closeBurgerNav={closeBurgerNav}
+        burger={burger}
+      />
+      <HomeSections burger={burger} />
+      <Footer burger={burger} />
     </>
   );
 };
