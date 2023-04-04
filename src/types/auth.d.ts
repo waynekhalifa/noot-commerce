f4a81bf9a3390f4d77995bd74fa24c919d1a635c
belalMAli <belalmrali@gmail.com>
2023-04-04 09:18:26 +0200

    redirect after auth

---
+++ src/types/auth.d.ts
@@ -0,0 +1,12 @@
+export type LoginProps = {
+  username: string;
+  password: string;
+};
+export type SignupProps = {
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  password1: string;
+  password2: string;
+};
